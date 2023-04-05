68b205e602238bc7e0cbfabe77d7caf238b817f7
SHASHANK9743 <shashank22.1994@gmail.com>
2023-04-05 11:31:25 +0530

    Completed Question 1

---
+++ Arrays/SearchAnElementInArray.py
@@ -0,0 +1,35 @@
+"""
+Given an integer array and another integer element.
+The task is to find if the given element is present in array or not.
+
+Your Task:
+The task is to complete the function search() which takes the array arr[], its size N and the
+element X as inputs and returns the index of first occurrence of X in the given array.
+If the element X does not exist in the array, the function should return -1.
+"""
+import unittest
+
+
+class Solution:
+    # Complete the below function
+    def search(self, arr, N, X):
+        pos = -1
+        for i, element in enumerate(arr, 0):
+            if element == X:
+                pos = i
+                break
+        return pos
+
+
+class TestSolution(unittest.TestCase):
+    def test_case_1(self):
+        arr = [1, 2, 3, 4]
+        solution = Solution()
+        idx = solution.search(arr, 4, 3)
+        self.assertTrue(idx == 2)
+
+    def test_case_2(self):
+        arr = [1, 2, 3, 4, 5]
+        solution = Solution()
+        idx = solution.search(arr, 5, 5)
+        self.assertTrue(idx == 4)
